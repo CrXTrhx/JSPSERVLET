36f89eacd929207bacbb6f71c76e465d1d8b0429
CrXTrhx <cristopherdmolina@gmail.com>
2023-03-28 23:37:42 -0600

    v 1.0

---
+++ src/java/org/cris/Datos.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.cris;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ruizl
+ */
+public class Datos implements Serializable{
+    private String nombre;
+    private float cal;
+    private String sexo;
+    private String email;
+    private String tel;
+    private String estado;
+    private Integer semestre;
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+
+    public Datos() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getCal() {
+        return cal;
+    }
+
+    public void setCal(float cal) {
+        this.cal = cal;
+    }
+    
+    
+    
+    
+}
